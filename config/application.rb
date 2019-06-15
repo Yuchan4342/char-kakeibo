d4f12d9861105d5af67935116110c4277d09271b
Yuto Iwata <jyuchan415@gmail.com>
2019-06-15 22:45:22 +0900

    タイムゾーンを Tokyo に設定. i18n の default_locale を ja に設定.

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,16 @@ module CharKakeibo
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+    # タイムゾーンを東京(JST +9:00)に設定
+    config.time_zone = 'Tokyo'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
   end
 end
