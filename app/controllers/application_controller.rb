8bcf7df6c84da5c5e34094b6402a85c9b75c6c8a
Yuto Iwata <jyuchan415@gmail.com>
2019-06-17 16:01:48 +0900

    ユーザ名を User model に追加できるように実装. ログイン中のユーザ名を navbar に表示.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # 新規登録/アカウント更新時(sign_up/account_update 時)に name というキーのパラメーターを追加で許可する.
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
