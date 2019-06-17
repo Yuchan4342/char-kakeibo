8fe47ced567a8f80e6694de1f18d350bbf149fe3
Yuto Iwata <jyuchan415@gmail.com>
2019-06-17 21:22:06 +0900

    TopController とトップ画面を作成して, root_path を top#index に設定.

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# TopController
+# アプリのトップ画面に関連する Controller.
+# ログイン不要.
+class TopController < ApplicationController
+  def index; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'purchases#index'
+  root to: 'top#index'
   resources :purchases, except: :show
   resources :categories, except: :show
   devise_for :users
