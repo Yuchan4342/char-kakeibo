a4fc29f7791532ab46dfe33bb79cd3469182f932
Yuto Iwata <jyuchan415@gmail.com>
2019-06-19 15:50:04 +0900

    set_search_date メソッドに一部動作を分離.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -7,16 +7,10 @@ class PurchasesController < ApplicationController
 
   before_action :authenticate_user!
   before_action :set_purchase, only: %i[edit update destroy]
+  before_action :set_search_date, only: [:index]
   before_action :create_default_category, only: %i[new edit]
 
   def index
-    search_params = params[:search]
-    @search_date = if search_params.nil?
-                     Time.zone.today
-                   else
-                     @search_date = Date.new(search_params[:"date(1i)"].to_i,
-                                             search_params[:"date(2i)"].to_i)
-                   end
     @purchases = Purchase.where(user: current_user,
                                 bought_at: @search_date.in_time_zone.all_month)
                          .eager_load(:category).order('bought_at DESC')
@@ -79,6 +73,17 @@ class PurchasesController < ApplicationController
 
     # purchases#index に飛ばす. TODO: i18n 対応.
     redirect_to(purchases_path, notice: '権限がありません。')
+  end
+
+  # パラメータから表示する月を設定する. パラメータがない場合は今月のデータを表示.
+  def set_search_date
+    search_params = params[:search]
+    @search_date = if search_params.nil?
+                     Time.zone.today
+                   else
+                     @search_date = Date.new(search_params[:"date(1i)"].to_i,
+                                             search_params[:"date(2i)"].to_i)
+                   end
   end
 
   def purchase_params
