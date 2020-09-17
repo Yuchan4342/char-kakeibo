c384239f5ed72044a4a46cf8d9f7cd099387935e
Yuto Iwata <jyuchan415@gmail.com>
2020-09-17 20:57:00 +0900

    pending を追加

--- spec/controllers/top_controller_spec.rb
+++ spec/controllers/top_controller_spec.rb
@@ -3,5 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe TopController, type: :controller do
-
+  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -3,5 +3,5 @@
 require 'rails_helper'
 
 RSpec.describe Users::RegistrationsController, type: :controller do
-
+  pending "add some examples to (or delete) #{__FILE__}"
 end
